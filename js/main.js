8286a542672334716333028fb5a94ab48264464a
Sarvarbbek0621 <majidovsarvar628@gmail.com>
2024-02-19 20:07:48 +0500

    Kayzo_MC_uz.aternos.me:45892

--- js/main.js
+++ js/main.js
@@ -1,86 +1,80 @@
-"use script"
+window.addEventListener("DOMContentLoaded", () => {
 
-// ! Categories start
-const categories = [
-    {
-        name: "Fertilizer",
-        img: "./images/category_1.jpg"
-    },
-    {
-        name: "Protective products and septic tanks",
-        img: "./images/category_2.png"
-    },
-    {
-        name: "Planting material",
-        img: "./images/category_3.png"
-    },
-    {
-        name: "Tools and equipment",
-        reyting: "2",
-        skidka: "60%",
-        img: "./images/category_4.png"
+    // ! Tabs
+
+    const tabContent = document.querySelectorAll(".categories_boxes"),
+        parentItem = document.querySelectorAll(".tabheader_items"),
+        tabHeaderItem = document.querySelectorAll(".tabheader_item");
+
+    function hideTabContent() {
+        tabContent.forEach(item => {
+            item.classList.add("hide");
+            item.classList.remove("show");
+        })
+
+        tabHeaderItem.forEach(item => {
+            item.classList.remove("tabheader_item__active");
+        })
     }
-];
 
-const categoriesBoxes = document.querySelector(".categories_boxes")
+    function showTabContent(i = 0) {
+        tabContent.forEach(item => {
+            tabContent[i].classList.remove("hide");
+            tabContent[i].classList.add("show_two");
 
-categories.forEach((item, indx) => {
-    categoriesBoxes.innerHTML += `
-    <div class="categorie-box">
-		<img src="${item.img}" alt="category_1">
-		<h4>${item.name}</h4>
-    </div>
-    `
-});
-// * Categories end
-
-
-// ! Sale start
-const saleProducts = [
-    {
-        name: "Decorative forged ...",
-        oldPrice: "$1000",
-        newPrice: "$500",
-        discount: "-50%",
-        img: "./images/sale_1.png"
-    },
-    {
-        name: "Flower basket",
-        oldPrice: "$150",
-        newPrice: "$100",
-        discount: "-34%",
-        img: "./images/sale_2.png"
-    },
-    {
-        name: "Aquarium lock",
-        oldPrice: "$200",
-        newPrice: "$150",
-        discount: "-25%",
-        img: "./images/sale_3.png"
-    },
-    {
-        name: "Secateurs",
-        oldPrice: "$240",
-        newPrice: "$199",
-        discount: "-50%",
-        img: "./images/sale_4.png"
+            tabHeaderItem[0].classList.add("tabheader_item__active")
+        })
     }
-];
+    hideTabContent();
+    showTabContent();
 
-const saleBoxes = document.querySelector(".sale_boxes")
+    parentItem.addEventListener('click', (event) => {
+        console.log(event);
+    })
 
-saleProducts.forEach((saleBox, indx) => {
-    saleBoxes.innerHTML += `
-    <div class="sale_box">
-		<img src="${saleBox.img}" alt="sale_1">
-		<div class="sale_texts">
-			<h4>${saleBox.name}</h4>
-			<h2 class="h_two">${saleBox.newPrice}</h2>
-			<del>${saleBox.oldPrice}</del>
-		</div>
-		<button class="btn btn_position">${saleBox.discount}</button>
-	</div>
-    `
-});
-
-
+    // ! Sale start
+    const saleProducts = [
+        {
+            name: "Decorative forged ...",
+            oldPrice: "$1000",
+            newPrice: "$500",
+            discount: "-50%",
+            img: "./images/sale_1.png"
+        },
+        {
+            name: "Flower basket",
+            oldPrice: "$150",
+            newPrice: "$100",
+            discount: "-34%",
+            img: "./images/sale_2.png"
+        },
+        {
+            name: "Aquarium lock",
+            oldPrice: "$200",
+            newPrice: "$150",
+            discount: "-25%",
+            img: "./images/sale_3.png"
+        },
+        {
+            name: "Secateurs",
+            oldPrice: "$240",
+            newPrice: "$199",
+            discount: "-50%",
+            img: "./images/sale_4.png"
+        }
+    ];
+    const saleBoxes = document.querySelector(".sale_boxes")
+    saleProducts.forEach((saleBox, indx) => {
+        saleBoxes.innerHTML += `
+        <div class="sale_box">
+		    <img src="${saleBox.img}" alt="sale_1">
+		    <div class="sale_texts">
+		    	<h4>${saleBox.name}</h4>
+			    <h2 class="h_two">${saleBox.newPrice}</h2>
+		    	<del>${saleBox.oldPrice}</del>
+		    </div>
+		    <button class="btn btn_position">${saleBox.discount}</button>
+	    </div>
+         `
+    });
+})
